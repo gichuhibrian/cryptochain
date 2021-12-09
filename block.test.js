857be0e54805c28e7c2a992d00e8bd4bd0010752
Brian Chege <brianchegegichuhi@gmail.com>
2021-12-09 04:03:56 +0300

    Created block test file

---
+++ block.test.js
@@ -0,0 +1,16 @@
+describe('Block', () => {
+    const timestamp = 'a-date'
+    const lastHash = 'foo-hash'
+    const hash = 'bar-hash'
+    const data = ['Blockchain', 'cryptochain']
+    const block = new Block({
+        timestamp, lastHash, hash, data
+    })
+
+    it('has a timestamp, lastHash, hash, and data', () => {
+        expect(block.timestamp).toEqual(timestamp)
+        expect(block.lastHash).toEqual(lastHash)
+        expect(block.hash).toEqual(hash)
+        expect(block.data).toEqual(data)
+    })
+})
