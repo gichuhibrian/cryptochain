fba5043b45df7f85b5a9e11845e2aa3e7a774943
Brian Chege <brianchegegichuhi@gmail.com>
2021-12-10 23:53:00 +0300

    Implemented dynamic difficulty as well as adjusting difficulty in mineBlock

--- block.js
+++ block.js
@@ -16,14 +16,15 @@ class Block {
     }
 
     static mineBlock({lastBlock, data}) {
-        let timestamp, hash;
         const lastHash = lastBlock.hash
-        const {difficulty} = lastBlock
+        let timestamp, hash;
+        let {difficulty} = lastBlock
         let nonce = 0
 
         do {
             nonce++
             timestamp = Date.now()
+            difficulty = Block.adjustDifficulty({originalBlock: lastBlock, timestamp})
             hash = cryptoHash(timestamp, lastHash, data, difficulty, nonce)
         } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
 
@@ -39,7 +40,11 @@ class Block {
 
     static adjustDifficulty({originalBlock, timestamp}) {
         const { difficulty } = originalBlock
+
+        if(difficulty < 1 ) return 1
+
         if(timestamp - originalBlock.timestamp > MINE_RATE ) return difficulty - 1
+        
         return difficulty + 1
     }
 }
