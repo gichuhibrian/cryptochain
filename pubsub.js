b7bdc69f67aff7a8065608c74f0ef669a7f2015d
Brian Chege <brianchegegichuhi@gmail.com>
2021-12-12 21:12:01 +0300

    Added Publisher Subscriber classes in both redis and pubnub

--- pubsub.js
+++ pubsub.js
@@ -1,10 +1,4 @@
-const PubNub = require('pubnub');
-
-const credentials = {
-    publishKey: 'pub-c-4c0ff8a3-58d3-4bcd-a869-21e2b6a8b7c9',
-    subscribeKey: 'sub-c-fd9e8eec-5b71-11ec-b9bb-4ee50875e851',
-    secretKey: 'sec-c-YjE3YzBiYzYtYjg0Yy00N2Y3LWFjODYtYzlkMjkxNmMyY2U5'
-}
+const redis = require('redis')
 
 const CHANNELS = {
     TEST: 'TEST'
@@ -12,28 +6,18 @@ const CHANNELS = {
 
 class PubSub {
     constructor() {
-        this.pubnub = new PubNub(credentials)
+        this.publisher = redis.createClient();
+        this.subscriber = redis.createClient();
 
-        this.pubnub.subscribe({channels: Object.values(CHANNELS)})
-
-        this.pubnub.addListener(this.listener)
+        this.subscriber.subscribe(CHANNELS.TEST)
+        this.subscriber.on('message', (channel, message) => this.handleMessage)
     }
 
-    listener() {
-        return {
-            message: messageObject => {
-                const {channel, message} = messageObject
-
-                console.log(`Message received. Channel: ${channel}. Message: ${message}`)
-            }
-        }
-    }
-
-    publish({ channel, message }) {
-      this.pubnub.publish({ channel, message })
+    handleMessage(channel, message) {
+        console.log(`Message received. Channel: ${channel}. Message: ${message}`)
     }
 }
 
-const testPubSub = new PubSub()
-testPubSub.publish({channel: CHANNELS.TEST, message: 'hello pubnub' })
-module.exports = PubSub
+const testPubSub = new PubSub();
+
+setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000)
