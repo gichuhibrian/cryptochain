a63aa9e92d079c8b900ea68a8dbbcf390114987c
Brian Chege <brianchegegichuhi@gmail.com>
2021-12-13 00:17:26 +0300

    Broadcasting of addedBlocks to the other nodes

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const Blockchain = require('./blockchain')
+const PubSub = require("./pubsub");
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain })
+
+const DEFAULT_PORT = 3000
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
+
+setTimeout(() => (pubsub.broadcastChain()), 1000)
 
 app.use(bodyParser.json());
 
@@ -14,11 +21,22 @@ app.get('/api/blocks', (req,res) => {
 app.post('/api/mine', (req, res) => {
     const { data } = req.body
     blockchain.addBlock({ data })
+    pubsub.broadcastChain()
 
     res.redirect('/api/blocks')
 })
 
-const PORT = 3000
+const syncChain = () => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/blocks`})
+}
+
+let PEER_PORT;
+
+if(process.env.GENERATE_PEER_PORT === 'true') {
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT
 app.listen(PORT, () => {
     console.log(`listening on localhost:${PORT}`)
 })
